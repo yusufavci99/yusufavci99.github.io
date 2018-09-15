d273659a972af2d5abbb523ef2e0ba982004f5fe
yusufavci99 <30696635+yusufavci99@users.noreply.github.com>
2018-09-15 04:19:06 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -33,7 +33,7 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+  myHeading.textContent = 'Welcome, ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
